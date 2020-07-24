f664cb356a175cce3346f5e6e672e47455c80753
Thomas Falcone <32480223+ThomasF34@users.noreply.github.com>
2020-07-24 11:03:21 +0200

    :tada: Initial Commit :tada:

---
+++ src/config.ts
@@ -0,0 +1,19 @@
+import { config } from 'dotenv';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+
+config();
+const defaultConfig = JSON.parse(readFileSync(resolve(__dirname, '../config.json')).toString());
+
+export interface IConfig {
+  PORT: number;
+  express_debug: boolean;
+}
+
+export function configuration(): IConfig {
+  const result: any = { ...defaultConfig };
+  for (const key in result) {
+    if (key in process.env) result[key] = process.env[key];
+  }
+  return result;
+}
