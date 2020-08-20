4106c7c1b88417bd8feccf15f26318809ae0ad75
Thomas Falcone <32480223+ThomasF34@users.noreply.github.com>
2020-08-20 17:21:00 +0200

    I3/J14 Fix user route returning IProfile

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,15 +1,18 @@
 import { Request, Response, NextFunction, Router } from 'express';
 import passport from 'passport';
+import { User } from '../models/users';
 
 export const router = Router();
 
 router.post('/', (req: Request, res: Response, next: NextFunction): void => {
-  passport.authenticate('local', function (err, user) {
+  passport.authenticate('local', function (err, userId) {
     if (err) return next(err);
-    if (!user) return res.status(401).send();
-    req.logIn(user, (err) => {
+    if (!userId) return res.status(401).send();
+    req.logIn(userId, async (err) => {
       if (err) return next(err);
-      res.json(user);
+      const user = await User.findById(userId);
+      if (!user) throw Error('User not found');
+      res.json(user.getSafeProfile());
     });
   })(req, res, next);
 });
